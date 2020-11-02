faf56a5f125351296a50da1afbf2987dcbeb7c53
Zero Ng <zero.ng@digitcube.com>
2020-11-02 11:57:26 +0800

    add orderService

--- nodejs/controllers/orderController.js
+++ nodejs/controllers/orderController.js
@@ -1,64 +1,73 @@
 const db = require("../models");
-const Order = db.order;
+const OrderService = require("../service/orderService.js");
 const Op = db.Sequelize.Op;
 const googleMapAPI = require("../controllers/googleMapAPI.js");
 const orderStatus = require("../models/orderStatus.js")
 
 exports.createOrder = async (req, res) => {
-    const inputArrayLength = 2;
-    // Validate request
-    if(!req.body.origin || !req.body.destination){
-        res.status(400).send({"error": "data not provided"});
-        return;
-    }
-    let isInputDataValid = true;
-    [req.body.origin,req.body.destination].forEach(elements=>{
-        if(!Array.isArray(elements)) isInputDataValid = false;                          //is array
-        if(elements.length !== inputArrayLength ) isInputDataValid = false;             //array length
-        for(let cur = 0;cur<inputArrayLength;cur++){
-            if(typeof(elements[cur])!=='string') isInputDataValid = false;             //array elements == string
-            if(isNaN(elements[cur])) isInputDataValid = false;                  //array elements is NOT A NUMBER
-        }
-    });
-
-    if(!isInputDataValid){
-        res.status(400).send({"error": "data invalid"});
-        return;
-    }
-
-    
-    let locationData = {
-        START_LATITUDE : req.body.origin[0],
-        START_LONGITUDE : req.body.origin[1],
-        END_LATITUDE : req.body.destination[0],
-        END_LONGITUDE : req.body.destination[1],
-    }
-
-    let response;
-    let responseJson;
-
+  // Validate request
     try{
-        response = await googleMapAPI.requestDistance(locationData);    //wait for the fetch distance result
-        if(!response.ok)                                                //http response checking
+      const inputArrayLength = 2;
+      if(!req.body.origin || !req.body.destination){
+          res.status(400).send({"error": "data not provided"});
+          return;
+      }
+      let isInputDataValid = true;
+      [req.body.origin,req.body.destination].forEach(elements=>{
+          if(!Array.isArray(elements)) isInputDataValid = false;                          //is array
+          if(elements.length !== inputArrayLength ) isInputDataValid = false;             //array length
+          for(let cur = 0;cur<inputArrayLength;cur++){
+              if(typeof(elements[cur])!=='string') isInputDataValid = false;             //array elements == string
+              if(isNaN(elements[cur])) isInputDataValid = false;                  //array elements is NOT A NUMBER
+          }
+      });
+
+      if(!isInputDataValid){
+          res.status(400).send({"error": "data invalid"});
+          return;
+      }
+    }catch(err){
+      res.status(500).send({"error":"data validation error"})
+    }
+
+
+    //get distance From Google MAP API
+    const distance = undefined;
+    try{
+        let locationData = {
+            START_LATITUDE : req.body.origin[0],
+            START_LONGITUDE : req.body.origin[1],
+            END_LATITUDE : req.body.destination[0],
+            END_LONGITUDE : req.body.destination[1],
+        }
+        const response = await googleMapAPI.requestDistance(locationData);    //wait for the fetch distance result
+        if(!response.ok)                                                       //http response checking
             throw new Error('request fail');
 
-        responseJson = await response.json();
+        const responseJson = await response.json();
         console.log(`fetch response ${JSON.stringify(responseJson)}`);
-        if(responseJson.status !== 'OK' || responseJson.rows[0].elements[0].status !== 'OK')                                //API endpoint status checking
+        if(responseJson.status !== 'OK' || responseJson.rows[0].elements[0].status !== 'OK')      //API endpoint status checking
             throw new Error('API endpoint return fail');
         if(!responseJson.rows[0].elements[0].distance.value)
             throw new Error('API endpoint does not contain distance value')
+        distance = responseJson.rows[0].elements[0].distance.value;
     }catch(err){
         console.log(`fetch distance error ${err.toString()}`)
         res.status(500).send({"error": "Error to get distance"});
         return;
     }
-    
-    locationData.distance = responseJson.rows[0].elements[0].distance.value;    //set distance value to data object for insert to DB
 
+    // insert Data To DB
     try{
-      let createResult = await Order.create(locationData);
-      console.log(`Order insertId ${createResult.order_id}`)
+      const createResult = OrderService.createOrder(
+        locationData.START_LATITUDE,
+        locationData.START_LONGITUDE,
+        locationData.END_LATITUDE,
+        locationData.END_LONGITUDE,
+        distance
+        );
+        
+      console.log(`Order insertId ${createResult.order_id}`);
       res.status(200).send({
               "id": createResult.order_id,
               "distance": createResult.distance,
@@ -76,34 +85,36 @@ exports.createOrder = async (req, res) => {
 
 // Retrieve all Orders from the database Limit by pages&limit
 exports.getList = async (req, res) => {
-  if(!req.query.page || !req.query.limit){
-    res.status(400).send({"error": "data not provided"});
-    return;
-  }
-
-  if(isNaN(req.query.page)|| isNaN(req.query.limit)){
-    res.status(400).send({"error": "data invalid"});
-    return;
-  }
-
-  if(parseInt(req.query.page)<=0 || parseInt(req.query.limit)<=0){
-    res.status(400).send({"error": "data invalid"});
-    return;
-  }
-
-  const limit = parseInt(req.query.limit);
-  const offset = parseInt(req.query.limit)*(parseInt(req.query.page)-1);
-  console.log(`select with limit ${limit} offset ${offset}`)
-
+  // Validate request
   try{
-    let findResult = await Order.findAll({
-      limit : limit,
-      offset : offset,
-      attributes: ['order_id','distance','order_status']
-    })
+    if(!req.query.page || !req.query.limit){
+      res.status(400).send({"error": "data not provided"});
+      return;
+    }
 
+    if(isNaN(req.query.page)|| isNaN(req.query.limit)){
+      res.status(400).send({"error": "data invalid"});
+      return;
+    }
+
+    if(parseInt(req.query.page)<=0 || parseInt(req.query.limit)<=0){
+      res.status(400).send({"error": "data invalid"});
+      return;
+    }
+  }catch(err){
+    res.status(500).send({"error":"data validation error"})
+  }
+
+
+  //fine all from db
+  try{
+    const limit = parseInt(req.query.limit);
+    const offset = parseInt(req.query.limit)*(parseInt(req.query.page)-1);
+    console.log(`select with limit ${limit} offset ${offset}`)
+
+    let findResult = OrderService.findAll(limit,offset);
     res.status(200).send( findResult.map(row=>{ 
-      row.order_status = orderStatus.toString(row.order_status);
+      row.order_status = orderStatus.toString(row.order_status);      //map int status => String status
       return row;
     }));
     
@@ -117,39 +128,47 @@ exports.getList = async (req, res) => {
 
 // Update a Tutorial by the id in the request
 exports.takeOrder = async (req, res) => {
-  if(!req.params.id || isNaN(req.params.id)){
-    res.status(400).send({"error": "data invalid"});
-    return;
-  }
-  if(!req.body.status || req.body.status!=='TAKEN'){
-    res.status(400).send({"error": "data invalid"});
-    return;
-  }
-  
-  const order_id = req.params.id;
+  // Validate request
   try{
+    if(!req.params.id || isNaN(req.params.id)){
+      res.status(400).send({"error": "data invalid"});
+      return;
+    }
+    if(!req.body.status || req.body.status!=='TAKEN'){
+      res.status(400).send({"error": "data invalid"});
+      return;
+    }
+  }catch(err){
+    res.status(500).send({"error":"data validation error"})
+  }
+
+  //Find and update status in DB
+  try{
+    const order_id = req.params.id;
     let findResult = await Order.findOne({
       where:{order_id:order_id}
     })
     
     if(findResult===null){
+      console.log(`orderID ${order_id} not found in db`);
       res.status(400).send({"error": "order not found"})
     }
 
     switch(findResult.order_status){
       case orderStatus.ENUM.UNASSIGN:
-            let result = await Order.update({order_status:orderStatus.ENUM.TAKEN},{where:{order_id:order_id}})
-            console.log(result);
+            let updateResult = await Order.update({order_status:orderStatus.ENUM.TAKEN},{where:{order_id:order_id}})
+            console.log(`updated order status to ${updateResult}`);
             res.status(200).send({"status": "SUCCESS"})
           break;
-        case orderStatus.ENUM.TAKEN:
-          res.status(400).send({"error": "order already taken"});
+      case orderStatus.ENUM.TAKEN:
+        res.status(400).send({"error": "order already taken"});
+        return;
+      case orderStatus.ENUM.OTHERS:
+        res.status(400).send({"error": "order can not take now"});
+        return;
+        default:
+          res.status(400).send({"error": "unknown order status"});
           return;
-          break;
-        case orderStatus.ENUM.OTHERS:
-          res.status(400).send({"error": "order can not take now"});
-          return;
-          break;
     }
     
 
---
+++ nodejs/service/orderService.js
@@ -0,0 +1,34 @@
+const db = require('../models')
+const Order = db.Order
+const orderService = {
+    findOne :async (orderID)=>{
+        const findOneResult = await Order.findOne({
+            where:{order_id:orderID}
+        });
+        return findOneResult;
+    },
+    findAll :async (limit,offset)=>{
+        const findAllResult = await Order.findAll({
+            limit : limit,
+            offset : offset,
+            attributes: ['order_id','distance','order_status']
+          })
+        return findAllResult;
+    },
+    create:async (START_LATITUDE,START_LONGITUDE,END_LATITUDE,END_LONGITUDE,distance)=>{
+        const createResult = await Order.create({
+            START_LATITUDE:START_LATITUDE,
+            START_LONGITUDE:START_LONGITUDE,
+            END_LATITUDE:END_LATITUDE,
+            END_LONGITUDE:END_LONGITUDE,
+            distance:distance
+        })
+        return createResult;
+    },
+    update:async(updateThings,condition)=>{
+        const updateResult = await Order.update(updateThings,condition);
+        return updateResult;
+    }
+};
+
+module.exports = orderService
