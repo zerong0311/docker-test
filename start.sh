6f8fbeb61394f20decc155debce8d055f2e55b51
Zero <zero@Zerode-MacBook-Pro.local>
2020-11-02 22:38:48 +0800

    update start script,update dockercompose image update init db sql

--- start.sh
+++ start.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-currntDir=$PWD
-volumeDataDir=${currntDir}'/data'
-isDataExist=false
-
-ECHO ${currntDir};
-ECHO ${volumeDataDir};
-ECHO ${isDataExist};
-
-#check if data is exist 
-if [ -d "${volumeDataDir}" ]; then
-  ECHO "${volumeDataDir} FOUND"
-else
-  ECHO "${volumeDataDir} NOT FOUND"
-  exit 1
-fi
+docker-compose up -d
