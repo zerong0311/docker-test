6c024155b1ae9c682cc062259317a1c3e521f2d8
Zero Ng <zero.ng@digitcube.com>
2020-11-03 09:09:13 +0800

    move api resp check to mapapi

--- nodejs/controllers/googleMapAPI.js
+++ nodejs/controllers/googleMapAPI.js
@@ -4,18 +4,40 @@ const fetch = require('node-fetch');
 const APIEndPoint = 'https://maps.googleapis.com/maps/api/distancematrix/';
 const format = 'json';
 
-async function requestDistance(data){
-    const APIURI = getAPIURI(data);
+async function requestDistance(START_LATITUDE,START_LONGITUDE,END_LATITUDE,END_LONGITUDE){
+    const APIURI = getAPIURI(START_LATITUDE,START_LONGITUDE,END_LATITUDE,END_LONGITUDE);
     console.log(`fetch googlemap API - ${APIURI}`);
 
     const response = await fetch(APIURI, { method: 'GET'});
-    return response;
+    const responseJson = await response.json();
+
+    if(responseJson.status !== 'OK')      //API endpoint status checking
+        throw new Error(`API endpoint return fail ,status != OK ${responseJson}`);
+
+    let returnResult = [];
+    responseJson.rows.forEach(elements =>{                  // loop all rows result;
+        console.log(`looping elements ${elements}`)
+
+        //make object loop as array
+        for(var key in elements){
+            if (!elements.hasOwnProperty(key)) continue;
+            var obj = elements[key];
+            obj.forEach(valueInElements => {
+                console.log(valueInElements);
+                if(valueInElements.status === 'OK')
+                returnResult.push(value.distance.value);
+            });
+        }
+    })
+    console.log(returnResult);
+
+    return returnResult;
 }
 
 
 //sample https://maps.googleapis.com/maps/api/distancematrix/json?origins=41.43206,-81.38992&destinations=42,-80&key=xxxxxxxxxxxxxxxxx
-function getAPIURI(data){
-    return `${APIEndPoint}${format}?origins=${data.START_LATITUDE},${data.START_LONGITUDE}&destinations=${data.END_LATITUDE},${data.END_LONGITUDE}&key=${APIConfig.APIKEY}`;
+function getAPIURI(START_LATITUDE,START_LONGITUDE,END_LATITUDE,END_LONGITUDE){
+    return `${APIEndPoint}${format}?origins=${START_LATITUDE},${START_LONGITUDE}&destinations=${END_LATITUDE},${END_LONGITUDE}&key=${APIConfig.APIKEY}`;
 }
 
 module.exports.requestDistance = requestDistance;
